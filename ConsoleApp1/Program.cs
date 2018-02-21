42be93e658d035148c567d7655bddf13bd2da3ed
Vadim Bolshakov <vadim_bolshakov@epam.com>
2018-02-21 19:17:10 +0300

    init

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConsoleApp1
@@ -10,6 +11,10 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            var guid = Guid.NewGuid();
+            var mutex = new Mutex(true, guid.ToString());
+            Console.WriteLine($"Opened mutex {guid} and wait.");
+            Console.ReadLine();
         }
     }
 }
---
+++ WorkerProcess/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WorkerProcess
+{
+    class Program
+    {
+        private static CancellationTokenSource cts = new CancellationTokenSource();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter mutex id");
+            var readLine = Console.ReadLine();
+            var mutexAquired = Mutex.TryOpenExisting(readLine, out var mutex);
+            Task workTask;
+            if (mutexAquired)
+            {
+                 workTask = Task.Run(() =>
+                {
+                    while (true)
+                    {
+                        Console.WriteLine("Working");
+                        Thread.Sleep(1000);
+
+                        if (cts.Token.IsCancellationRequested)
+                        {
+                            cts.Token.ThrowIfCancellationRequested();
+                        }
+                    }
+                }, cts.Token).ContinueWith(task=> Console.WriteLine("All work is done"),TaskContinuationOptions.None);
+
+                try
+                {
+                    mutex.WaitOne();
+                }
+                catch (AbandonedMutexException e)
+                {
+                    Console.WriteLine(e);
+                    Console.WriteLine("Kill switch flipped! Host process exited. ");
+                }
+
+                cts.Cancel();
+                Console.WriteLine("Cancelation");
+                workTask.Wait();
+            }
+            else
+            {
+                Console.WriteLine("Can't enter mutex lock");
+            }
+
+            Console.WriteLine("WorkerProcess exiting");
+            Console.ReadLine();
+            Environment.Exit(0);
+        }
+    }
+}
